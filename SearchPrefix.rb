0633621023d33c821262a3f0943cc31e3dc24310
aoyama11 <aoyama1@kenyon.edu>
2024-02-07 14:54:36 -0500

    completed

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,22 @@
 def searchPrefix(arr,s)
-
+    array = []
+    prelen = s.length()
+    seperate = s.split("")
+    arr.each do |word|
+        letters = word.split("")
+        if prelen > 1
+            if letters[0] == seperate[0] and letters[1] == seperate[1]
+                array.push(word)
+            end
+        else
+            if letters[0] == seperate[0]
+                array.push(word)
+            end
+        end
+    end
+    print array
 end
 
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
+puts(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
 
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+puts(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
